9e9dd25bfafea1dfc6f4e77107aac8ace6c952a3
jascha.merle <jascha.merle@epita.fr>
2021-07-24 13:49:02 +0200

    First push

---
+++ MacOS/EPILANGAGE/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  EPILANGAGE
+//
+//  Created by Jascha on 24/07/2021.
+//  Copyright © 2021 Jascha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
